648d0b5e90789d2114c894847fc984d44eab870a
TheaWu7 <wudeyu0411@163.com>
2021-12-20 21:11:36 +0800

    axios 并发请求

--- 18-axios/src/main.js
+++ 18-axios/src/main.js
@@ -7,7 +7,7 @@ new Vue({
   render: (h) => h(App),
 }).$mount("#app");
 
-// axios 基本使用
+// 1. axios 基本使用
 axios({
   // 默认get
   url: "http://123.207.32.32:8000/home/multidata",
@@ -30,3 +30,31 @@ axios({
 }).then((res) => {
   console.log(res);
 });
+
+// 2. 并发请求
+axios
+  .all([
+    axios({
+      url: "http://123.207.32.32:8000/home/multidata",
+    }),
+    axios({
+      url: "http://123.207.32.32:8000/home/data",
+      params: {
+        type: "pop",
+        page: 1,
+      },
+    }),
+  ])
+  // 返回数组
+  .then((res) => {
+    console.log(res);
+    console.log(res[0]);
+    console.log(res[1]);
+  });
+// 用spread两个分开返回
+// .then(
+//   axios.spread((res1, res2) => {
+//     console.log(res1);
+//     console.log(res2);
+//   })
+// );
